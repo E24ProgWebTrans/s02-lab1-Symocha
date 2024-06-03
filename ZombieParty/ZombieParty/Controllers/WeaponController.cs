b7a226f618cfadfa323be423cbd9f102d3144b3f
Symocha <1843999@cegepmontpetit.ca>
2024-06-03 15:20:21 -0400

    FCT Weapon Index et Create
    Maintenant possible de voir la liste de Weapons et d’en ajouter.

---
+++ ZombieParty/ZombieParty/Controllers/WeaponController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZombieParty.Models;
+
+
+namespace ZombieParty.Controllers
+{
+    public class WeaponController : Controller
+    {
+        private BaseDonnees _baseDonnees { get; set; }
+
+        public WeaponController(BaseDonnees baseDonnees)
+        {
+            _baseDonnees = baseDonnees;
+        }
+        public IActionResult Index()
+        {
+            this.ViewBag.MaListe = _baseDonnees.Weapons.ToList();
+            return View(this.ViewBag.MaListe);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Weapon weapon)
+        {
+            _baseDonnees.Weapons.Add(weapon);
+            TempData["Success"] = $"Zombie {weapon.Name} added";
+            return this.RedirectToAction("Index");
+        }
+    }
+}
---
+++ ZombieParty/ZombieParty/Models/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ZombieParty.Models
+{
+    public class Weapon
+    {
+        [Required]
+        [MaxLength(250)]
+        [MinLength(2)]
+        public string Name { get; set; }       
+        [MaxLength(2500)]
+        [MinLength(0)]
+        public string? Description { get; set; }
+        [Range(0,500)]
+        public decimal Force { get; set; }
+        [Range(0, 100000,ErrorMessage = "The <PRICE> has to be between 0 and 100 000")]
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string? Image { get; set; }
+        public int Qty { get; set; }
+        public int QtyBought { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var item = validationContext.ObjectInstance as Weapon;
+            if (item == null) yield break;
+            if (string.IsNullOrWhiteSpace(item.Description)) yield break;
+            if (item.Description.Split(" ").Length <= 3)
+                yield return new ValidationResult("Description needs to have more than 3 words please.", new[] { "Description" });
+        }
+
+    }
+
+}
